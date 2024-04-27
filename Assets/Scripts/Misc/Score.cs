07cd858117f3fa0069e20611ba0e2fe316da9826
Muhammet Ali <mgozalann@gmail.com>
2024-04-27 20:21:22 +0300

    last commit

---
+++ Assets/Scripts/Misc/Score.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    private int _currentScore = 0;
+    private TMP_Text _scoreText;
+
+    private void Awake() {
+        _scoreText = GetComponent<TMP_Text>();
+    }
+
+    private void OnEnable() {
+        Health.OnDeath += EnemyDestroyed;
+    }
+
+    private void OnDisable() {
+        Health.OnDeath -= EnemyDestroyed;
+    }
+
+    private void EnemyDestroyed(Health sender) {
+        _currentScore++;
+        _scoreText.text = _currentScore.ToString("D3");
+    }
+}
